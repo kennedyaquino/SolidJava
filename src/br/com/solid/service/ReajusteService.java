2f894eb3ae03bf2057f99655727ffc3e1e9ddbb1
kennedyaquino <kennedy.eletronica.94@gmail.com>
2022-10-16 14:06:00 -0300

    adicionado mais uma regra de negocio

--- src/br/com/solid/service/ReajusteService.java
+++ src/br/com/solid/service/ReajusteService.java
@@ -2,6 +2,8 @@ package br.com.solid.service;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 import br.com.solid.ValidacaoException;
 import br.com.solid.model.Funcionario;
@@ -14,6 +16,15 @@ public class ReajusteService {
 		if(percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
 			throw new ValidacaoException("Reajuste não pode ser superior a 40% do salario");
 		}
+		
+		LocalDate dataUltimoReajuste = funcionario.getDataUltimoReajuste();
+		LocalDate dataAtual = LocalDate.now();
+		long mesesDesdeUltimoReajuste = ChronoUnit.MONTHS.between(dataUltimoReajuste, dataAtual);
+		
+		if(mesesDesdeUltimoReajuste < 6) {
+			throw new ValidacaoException("Intervalo entre reajustes deve ser de no minimo 6 meses.");
+		}
+		
 		BigDecimal salarioReajustado = salarioAtual.add(aumento);
 		funcionario.atualizarSalario(salarioReajustado);
 	}
