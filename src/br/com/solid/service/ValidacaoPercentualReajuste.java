76ef6cedf2f91b683b2a7c675f2512df17b86c93
kennedyaquino <kennedy.eletronica.94@gmail.com>
2022-10-16 14:43:57 -0300

    correcao classe ValidacaoPercentua

--- src/br/com/solid/service/ValidacaoPercentualReajuste.java
+++ src/br/com/solid/service/ValidacaoPercentualReajuste.java
@@ -10,7 +10,7 @@ public class ValidacaoPercentualReajuste implements ValidacaoReajuste {
 	
 	@Override
 	public void validar(Funcionario funcionario, BigDecimal aumento) {
-		BigDecimal salarioAtual = funcionario.getSalario();
+		BigDecimal salarioAtual = funcionario.getDadosPessoais().getSalario();
 		BigDecimal percentualReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
 		if(percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
 			throw new ValidacaoException("Reajuste não pode ser superior a 40% do salario");
